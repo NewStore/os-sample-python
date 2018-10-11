4e6a67a1ab225b3dd85630b095b2590f542b76c8
Jihyuk Bok <jhbok@newstore.com>
2018-10-11 15:55:33 +0200

    replica newstore

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,11 @@
+from requests import get
 from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hey NewStore! Hello World!"
+    return get('https://www.newstore.com').text
 
 if __name__ == "__main__":
     application.run()
+    
